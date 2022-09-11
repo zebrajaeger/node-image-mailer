bf123362b8519fa5cb68dfae2f1e7a0e93974d38
Lars Brandt <zebrajaeger@zebrajaeger.de>
2022-09-11 21:10:18 +0200

    add logging fix some bugs make executable

--- index.js
+++ index.js
@@ -6,53 +6,32 @@ const fs = require('fs');
 const nodemailer = require("nodemailer");
 const {v4: uuidv4} = require('uuid');
 const commander = require('commander');
+const winston = require('winston')
 
 const credentials = require('./credentials.json');
 const packageJson = require('./package.json');
 
-// TODO add progress bar
-const cliProgress = require('cli-progress');
-// const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-// bar1.start(200, 0);
-// bar1.update(100);
-// bar1.stop();
+// logger
+const consoleTransport = new winston.transports.Console()
+const myformat = winston.format.combine(
+    winston.format.colorize({message: true, colors: {info: 'blue',}}),
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+const myWinstonOptions = {
+    transports: [new winston.transports.Console({
+        format: myformat
+    })]
+}
+const LOG = new winston.createLogger(myWinstonOptions)
 
-async function main() {
-    commander
-        .version(packageJson.version, '-v, --version')
-        .option('-d, --dry-run', 'Dont send mail')
-        .option('-t, --to <value>', 'Mail to.')
-        .option('-p, --path <value>', 'Files dir.', '.')
-        .option('-s, --subject <value>', 'Mail subject.')
-        .option('-m, --max-size <value>', 'Max attachment size (default; 8MB)', (8 * 1024 * 1024).toString())
-        .parse(process.argv);
-
-    const options = commander.opts();
-
-    const maxSize = Number.parseInt(options.maxSize);
-
-    let dirToSend = options.path
-    if (!path.isAbsolute(dirToSend)) {
-        console.log('debug: path is relative')
-        dirToSend = path.resolve(__dirname, dirToSend)
-    }
-    options.path = dirToSend
-
-    const filter = (/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i)
-    const from = credentials.user
-    const to = options.to
-    if (!to) {
-        throw '-t or --to option isrequired';
-    }
-    const subject = options.subject || path.parse(dirToSend).base;
-    options.subject = subject;
-
-    console.log({options})
-
+function generateEmailData(dirToSend, maxSize) {
     const mailsToSend = [];
     let mailCount = 1;
     const filesToSend = []
     let currentSize = 0;
+    const filter = (/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i)
     fs.readdirSync(dirToSend).forEach(f => {
         const file = path.resolve(dirToSend, f)
         if (filter.test(file)) {
@@ -62,7 +41,7 @@ async function main() {
                 mailsToSend.push({
                     attachments: [...filesToSend],
                     size: currentSize,
-                    sizeT: formatNumber(currentSize),
+                    sizeT: formatBytes(currentSize),
                     count: mailCount
                 })
                 mailCount++;
@@ -74,7 +53,7 @@ async function main() {
                     file: path.parse(file).base,
                     cid: uuidv4(),
                     size,
-                    sizeT: formatNumber(size)
+                    sizeT: formatBytes(size)
                 });
                 currentSize += size;
             }
@@ -85,12 +64,57 @@ async function main() {
         mailsToSend.push({
             attachments: [...filesToSend],
             size: currentSize,
-            sizeT: formatNumber(currentSize),
+            sizeT: formatBytes(currentSize),
             count: mailCount
         })
         currentSize = 0;
         filesToSend.length = 0
     }
+    return mailsToSend;
+}
+
+async function main() {
+    LOG.debug(process.argv)
+    let receiver = undefined;
+    commander
+        .version(packageJson.version, '-v, --version')
+        .option('-d, --dry-run', 'Dont send mail')
+        .option('-p, --path <value>', 'Files dir.', process.cwd())
+        .option('-s, --subject <value>', 'Mail subject.')
+        .option('-m, --max-size <value>', 'Max attachment size (default; 8MB)', (8 * 1024 * 1024).toString());
+    commander
+        .addArgument(new commander.Argument('<receiver>', 'Mail to. Mail or targets-name from credentials.json'))
+        .action((receiver_) => {
+            receiver = receiver_;
+        });
+    commander.parse();
+    const options = commander.opts();
+
+    // maxSize
+    const maxSize = Number.parseInt(options.maxSize);
+
+    // directory
+    let dirToSend = options.path
+    if (!path.isAbsolute(dirToSend)) {
+        LOG.info('path is relative')
+        dirToSend = path.resolve(__dirname, dirToSend)
+    }
+    options.path = dirToSend
+
+    // receiver
+    options.receiver = receiver
+    credentials.receivers?.forEach(receiver_ => {
+        if (receiver_.name === receiver && !!receiver_.email) {
+            options.receiver = receiver_.email;
+        }
+    })
+    LOG.info(`send to '${options.receiver}'`)
+
+    // subject
+    const subject = options.subject || path.parse(dirToSend).base;
+    options.subject = subject;
+    LOG.info(JSON.stringify(options))
+    const mailsToSend = generateEmailData(dirToSend, maxSize);
 
     mailsToSend.forEach(mail => {
         mail.subject = `${subject} (${mail.count}/${mailsToSend.length})`
@@ -98,14 +122,13 @@ async function main() {
         mail.attachments.forEach(attachment => {
             attachment.cid = uuidv4();
             let p = path.parse(attachment.file);
-            html += `${p.base}:<br><img src="cid:${attachment.cid}"><br><br>`
+            html += `${p.base}:<br><img src="cid:${attachment.cid}" alt="${p.base}"><br><br>`
         })
         mail.html = html;
     })
 
-    sendMails(mailsToSend, from, to, options.dryRun)
+    await sendMails(mailsToSend, credentials.user, options.receiver, options.dryRun)
 }
-
 
 async function sendMails(mails, from, to, dryRun) {
 
@@ -122,10 +145,11 @@ async function sendMails(mails, from, to, dryRun) {
     for (let mail of mails) {
         mail.to = to;
         mail.from = from;
-        console.log('send', mail);
+        LOG.info(JSON.stringify(mail, null, 4))
         if (!dryRun) {
-            const info = await transporter.sendMail(mail);
-            console.log("Message sent: %s", info.messageId);
+            await transporter.sendMail(mail, (err, info) => {
+                console.log(err, info)
+            });
         }
     }
 }
@@ -134,4 +158,20 @@ function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
 }
 
-main().catch(console.error);
+function formatBytes(bytes, decimals = 2) {
+    if (!+bytes) return '0 Bytes'
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
+if (require.main === module) {
+    main().catch(console.error);
+}
+
+module.exports = main
